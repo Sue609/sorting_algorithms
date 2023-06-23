4586bc78dbb6abe0c50196cb483167ac3b5dd5f2
Sue609 <susankamau609@gmail.com>
2023-06-23 06:01:37 -0700

    Write a function that sorts an array of integers in ascending order using the Selection sort algorithm.

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,7 +3,7 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - function that worts a doubly linked list of integers in ascending
+ * insertion_sort_list - function that worts a doubly linked list of integers.
  * order using the insert sort algorithm.
  * @list: The array of the list to be sorted.
  */
@@ -15,9 +15,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *prev;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
-	{
 		return;
-	}
 	current = (*list)->next;
 	while (current != NULL)
 	{
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,6 +14,8 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, min;
 	int temp;
 
+	if (array == NULL || size < 2)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
@@ -27,7 +29,7 @@ void selection_sort(int *array, size_t size)
 			temp = array[i];
 			array[i] = array[min];
 			array[min] = temp;
+			print_array(array, size);
 		}
-		print_array(array, size);
 	}
 }
