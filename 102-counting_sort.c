916fa716e1b37b1a5a885485e28dd4bf0b221b3e
Sue609 <susankamau609@gmail.com>
2023-06-23 07:12:25 -0700

    Write a function that sorts an array of integers in ascending order using the Counting sort algorithm.

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -11,49 +11,47 @@
 
 void counting_sort(int *array, size_t size)
 {
-	int max = array[0];
-	int *count, *sorted;
-	int j;
-	size_t i;
+	int max = 0;
+	int *count_array, *output;
+	int i;
 
-	if (array == NULL || size <= 1)
+	if (array == NULL || size < 2)
 		return;
 
-	for (i = 1; i < size; i++)
+	for (i = 0; i < (int)size; i++)
 	{
 		if (array[i] > max)
 			max = array[i];
 	}
-	count = malloc((max + 1) * sizeof(int));
-	if (count == NULL)
+	count_array = malloc((max + 1) * sizeof(int));
+	if (count_array == NULL)
 		return;
 
-	for (j = 0; j <= max; j++)
-		count[j] = 0;
+	for (i = 0; i <= max; i++)
+		count_array[i] = 0;
 
-	for (i = 0; i < size; i++)
-		count[array[i]]++;
+	for (i = 0; i < (int)size; i++)
+		count_array[array[i]]++;
 
-	for (j = 1; j <= max; j++)
-		count[j] += count[j - 1];
+	for (i = 1; i <= max; i++)
+		count_array[i] += count_array[i - 1];
 
-	sorted = malloc(size * sizeof(int));
-	if (sorted == NULL)
+	output = malloc(size * sizeof(int));
+	if (output == NULL)
 	{
-		free(count);
+		free(count_array);
 		return;
 	}
 
-	for (i = 0; i < size; i++)
+	for (i = (int)size - 1; i >= 0; i--)
 	{
-		sorted[count[array[i]] - 1] = array[i];
-		count[array[i]]--;
+		output[count_array[array[i]] - 1] = array[i];
+		count_array[array[i]]--;
 	}
-	for (i = 0; i < size; i++)
-		array[i] = sorted[i];
+	for (i = 0; i < (int)size; i++)
+		array[i] = output[i];
+	print_array(count_array, max + 1);
 
-	for (j = 0; j <= max; j++)
-		printf("%d%s", count[j], (j == max) ? "\n" : ", ");
-	free(count);
-	free(sorted);
+	free(count_array);
+	free(output);
 }
