b74969adf4e7a2f9412e0e36a77940ba31f64b08
Sue609 <susankamau609@gmail.com>
2023-06-25 21:36:48 -0700

    Optimization of the bubble sort algorithm by adding a flag variable that checks for swaps.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,17 +14,22 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int flag;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		flag = 0;
+		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap_elements(&array[j], &array[j + 1]);
 				print_array(array, size);
+				flag = 1;
 			}
 		}
+		if (flag == 0)
+			break;
 	}
 }
 
